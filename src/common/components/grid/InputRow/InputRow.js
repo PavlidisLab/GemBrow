58eff5a5ae93f2d6fd6de288ef45e488b273e087
Stepan Tesar <tesar.stepann@gmail.com>
2018-01-18 14:50:02 -0800

    Visual tweaks

--- src/common/components/grid/InputRow/InputRow.js
+++ src/common/components/grid/InputRow/InputRow.js
@@ -5,16 +5,16 @@ import "./inputRow.css"
 const InputRow = (props) => (
     <div className={"input-row "+(props.className?props.className:"")}>
         <Row>
-            <Col>
+            <Col className={"input-row-label " + (props.count ? "count" : "no-count")}>
                 <Label for={props.id}>{props.label}</Label>
             </Col>
             {props.count ?
-                <Col className={"input-row-count text-right"} >
+                <Col className={"input-row-count text-right "} >
                     {props.count}
                 </Col>
                 :''
             }
-            <Col className={"input-row-input text-right"} id={props.id}>
+            <Col className={"input-row-input text-right " + (props.count ? "count" : "no-count")} id={props.id}>
                 {props.input}
             </Col>
         </Row>
