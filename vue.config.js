1713ee4a0b70835bdba87cba2bda9b1bce082001
Stepan Tesar <tesar.stepann@gmail.com>
2018-06-08 14:07:17 -0700

    Add production baseUrl setting for current testing setup

---
+++ vue.config.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+// noinspection JSUnresolvedVariable
+module.exports = {
+  baseUrl:
+    process.env.NODE_ENV === "production"
+      ? "/resources/restapidocs/gembrow/"
+      : "/"
+};
