0c82f7d7b9d3498164b287da36d06ceb14f02b97
Stepan Tesar <tesar.stepann@gmail.com>
2018-10-01 14:14:18 -0700

    Change geeq score color palette

--- src/components/ViewUtils.vue
+++ src/components/ViewUtils.vue
@@ -2,16 +2,18 @@
 export default {
   methods: {
     scoreToIcon(score) {
-      return score > 0.3
+      return score > 0.45
         ? "mdi-emoticon-happy"
-        : score > -0.3
+        : score > 0.1
           ? "mdi-emoticon-neutral"
           : "mdi-emoticon-sad";
     },
     scoreToColor(score) {
-      // normalize from [-1,1] to [0,1]
-      score = score + 1;
-      score = score / 2;
+      score = score * 1.6; // Puts 0.3 almost in the middle (close enough)
+      // Cut off anything > 1
+      score = score > 1 ? 1 : score;
+      // Cut off negative values
+      score = score < 0 ? 0 : score;
       // hsl red = 0° and green = 120°
       const hue = score * 120;
       return "hsl(" + hue + ", 100%, 70%)";
