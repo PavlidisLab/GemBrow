4cd4300ed988d5ad9223b3a618c2451902d15aec
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2023-06-22 14:13:49 -0700

    Fill all the links in the app bar

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -14,10 +14,12 @@
         <v-spacer/>
         <v-menu open-on-hover offset-y>
             <template v-slot:activator="{on, attrs}">
-                <v-btn plain v-bind="attrs" v-on="on">
-                    Explore
-                    <v-icon>mdi-chevron-down</v-icon>
-                </v-btn>
+                <form method="get">
+                    <v-btn plain v-bind="attrs" v-on="on">
+                        Explore
+                        <v-icon>mdi-chevron-down</v-icon>
+                    </v-btn>
+                </form>
             </template>
             <v-list>
                 <v-list-item>
@@ -25,17 +27,11 @@
                     <v-btn>Go</v-btn>
                 </v-list-item>
                 <v-divider/>
-                <v-list-item link>
-                    <a :href="baseUrl + '/expressionExperiment/showAllExpressionExperiments.html'">
-                        Browse Datasets
-                    </a>
+                <v-list-item link :href="baseUrl + '/expressionExperiment/showAllExpressionExperiments.html'">
+                    Browse Datasets
                 </v-list-item>
-                <v-list-item link>
-                    <v-list-item-title>
-                        <a :href="baseUrl + '/arrays/showAllArrayDesigns.html'">
-                            Browse Platforms
-                        </a>
-                    </v-list-item-title>
+                <v-list-item link :href="baseUrl + '/arrays/showAllArrayDesigns.html'">
+                    Browse Platforms
                 </v-list-item>
             </v-list>
         </v-menu>
@@ -47,8 +43,8 @@
                 </v-btn>
             </template>
             <v-list>
-                <v-list-item link>About Gemma</v-list-item>
-                <v-list-item link>Help and Documentation</v-list-item>
+                <v-list-item link :href="baseUrl + '/'">About Gemma</v-list-item>
+                <v-list-item link href="https://pavlidislab.github.io/Gemma/">Help and Documentation</v-list-item>
             </v-list>
         </v-menu>
         <v-menu v-if="myself && (myself.group === 'Users' || myself.group === 'Administrators')" open-on-hover
@@ -60,12 +56,16 @@
                 </v-btn>
             </template>
             <v-list>
-                <v-list-item link>Load Data</v-list-item>
-                <v-list-item link>Dataset Manager</v-list-item>
-                <v-list-item link>Gene Groups</v-list-item>
-                <v-list-item link>Experiment Groups</v-list-item>
+                <v-list-item link :href="baseUrl + '/expressionExperiment/upload.html'">Load Data</v-list-item>
+                <v-list-item link
+                             :href="baseUrl + '/expressionExperiment/showAllExpressionExperimentLinkSummaries.html'">
+                    Dataset Manager
+                </v-list-item>
+                <v-list-item link :href="baseUrl + '/geneGroupManager.html'">Gene Groups</v-list-item>
+                <v-list-item link :href="baseUrl + '/expressionExperimentSetManager.html'">Experiment Groups
+                </v-list-item>
                 <v-divider/>
-                <v-list-item link>Edit Profile</v-list-item>
+                <v-list-item link :href="baseUrl + '/userProfile.html'">Edit Profile</v-list-item>
             </v-list>
         </v-menu>
         <v-menu v-if="myself && (myself.group === 'Administrators')" open-on-hover offset-y>
@@ -76,17 +76,17 @@
                 </v-btn>
             </template>
             <v-list>
-                <v-list-item link>Add Data</v-list-item>
-                <v-list-item link>Browse GEO</v-list-item>
-                <v-list-item link>Search Annotations</v-list-item>
+                <v-list-item link :href="baseUrl + '/admin/loadExpressionExperiment.html'">Add Data</v-list-item>
+                <v-list-item link :href="baseUrl + '/admin/geoRecordBrowser.html'">Browse GEO</v-list-item>
+                <v-list-item link :href="baseUrl + '/characteristicBrowser.html'">Search Annotations</v-list-item>
                 <v-divider/>
-                <v-list-item link>Manager Users</v-list-item>
-                <v-list-item link>View Active Sessions</v-list-item>
-                <v-list-item link>System Monitoring</v-list-item>
-                <v-list-item link>Manage Search Indexes</v-list-item>
-                <v-list-item link>Manage Maintenance Mode</v-list-item>
-                <v-list-item link>Update "What's New"</v-list-item>
-                <v-list-item link>Widget Test Page</v-list-item>
+                <v-list-item link :href="baseUrl + '/admin/userManager.html'">Manager Users</v-list-item>
+                <v-list-item link :href="baseUrl + '/admin/activeUsers.html'">View Active Sessions</v-list-item>
+                <v-list-item link :href="baseUrl + '/admin/systemStats.html'">System Monitoring</v-list-item>
+                <v-list-item link :href="baseUrl + '/admin/indexer.html'">Manage Search Indexes</v-list-item>
+                <v-list-item link :href="baseUrl + '/admin/maintenanceMode.html'">Manage Maintenance Mode</v-list-item>
+                <v-list-item link :href="baseUrl + '/whatsnew/generateCache.html'">Update "What's New"</v-list-item>
+                <v-list-item link :href="baseUrl + '/admin/widgetTest.html'">Widget Test Page</v-list-item>
             </v-list>
         </v-menu>
         <v-menu v-if="myself" open-on-hover offset-y>
@@ -97,8 +97,8 @@
                 </v-btn>
             </template>
             <v-list>
-                <v-list-item link>Edit your profile</v-list-item>
-                <v-list-item link>Logout</v-list-item>
+                <v-list-item link :href="baseUrl + '/userProfile.html'">Edit your profile</v-list-item>
+                <v-list-item link :href="baseUrl + '/'">Logout</v-list-item>
             </v-list>
         </v-menu>
         <a href="https://www.ubc.ca/" target="_blank" title="UBC home page">
