583bcaf798743e71c4f272e8e6b6344d0e2cf418
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2023-07-10 14:08:21 -0700

    More improvements for the About Gemma dialog

--- src/components/AboutDialog.vue
+++ src/components/AboutDialog.vue
@@ -1,7 +1,13 @@
 <template>
     <v-dialog :value="value" @input="newVal => $emit('input', newVal)" width="800">
         <v-card>
-            <v-card-title>About Gemma</v-card-title>
+            <v-card-title>
+                About Gemma
+                <v-spacer></v-spacer>
+                <v-btn icon @click="$emit('input', false)">
+                    <v-icon>mdi-close</v-icon>
+                </v-btn>
+            </v-card-title>
             <v-card-text>
                 <p>
                     Gemma is a web site, database and a set of tools for the meta-analysis, re-use and sharing of
@@ -9,29 +15,50 @@
                     contains data from thousands of public studies, referencing thousands of published papers. Users can
                     search, access and visualize coexpression and differential expression results. For more information,
                     see the
-                    <a href="https://pavlidislab.github.io/Gemma/" target="_blank">help and documentation <v-icon x-small>mdi-open-in-new</v-icon></a>.
+                    <a href="https://pavlidislab.github.io/Gemma/" target="_blank">
+                        help and documentation
+                        <v-icon x-small>mdi-open-in-new</v-icon>
+                    </a>.
                 </p>
                 <p>
-                    Gemma was developed by the Pavlidis group at UBC (<a href="https://pavlidislab.github.io/Gemma/#credits" target="_blank">credits&nbsp;<v-icon x-small>mdi-open-in-new</v-icon></a>).
+                    Gemma was developed by the Pavlidis group at UBC (<a
+                        href="https://pavlidislab.github.io/Gemma/#credits" target="_blank">credits&nbsp;<v-icon
+                        x-small>mdi-open-in-new
+                </v-icon>
+                </a>).
                 </p>
-                <p>
+                <p class="mb-0">
                     To cite Gemma, please use:<br>
                     Lim N., et al., Curation of over 10,000 transcriptomic studies to enable data reuse.
-                    <em>Database</em>, 2021. <a href="https://doi.org/10.1093/database/baab006" target="_blank">link&nbsp;<v-icon x-small>mdi-open-in-new</v-icon></a>
+                    <em>Database</em>, 2021.
+                    <a href="https://doi.org/10.1093/database/baab006" target="_blank">
+                        link
+                        <v-icon x-small>mdi-open-in-new</v-icon>
+                    </a>
                 </p>
-                <p>Gemma's expression platform and gene annotations are powered by:</p>
+                <p v-if="root.externalDatabases.length > 0" class="mt-3">Gemma's expression platform and gene
+                    annotations are powered by:</p>
                 <template v-for="ed in root.externalDatabases">
-                    <h4 :key="ed.id + '_dd'" class="text-capitalize">{{ ed.name }}</h4>
-                    <p :key="ed.id + '_dt'">
-                        {{ ed.description }}
-                        <a v-if="ed.uri" :href="ed.uri" target="_blank" rel="noreferrer noopener">link <v-icon x-small>mdi-open-in-new</v-icon></a><br>
+                    <h4 :key="ed.id + '_dd'" class="mt-3 text-capitalize">{{ ed.name }}</h4>
+                    <p :key="ed.id + '_dt'" class="mb-0">
+                        {{ m2h(ed.description) }}
+                        <a v-if="ed.uri" :href="ed.uri" target="_blank" rel="noreferrer noopener">link
+                            <v-icon x-small>mdi-open-in-new</v-icon>
+                        </a><br>
                         <span v-if="ed.releaseVersion">Release used: {{ ed.releaseVersion }}.<br></span>
                         <span v-if="ed.lastUpdated">Last updated on {{ new Date(ed.lastUpdated).toLocaleDateString() }}.<br></span>
                         <template>
                             <span v-for="rd in ed.externalDatabases" :key="rd.id">
-                                <span class="text-capitalize">{{ rd.name }}</span> <a v-if="rd.releaseUrl" :href="rd.releaseUrl" target="_blank" rel="noreferrer noopener">{{ rd.releaseVersion}} <v-icon x-small>mdi-open-in-new</v-icon></a>
+                                <span class="text-capitalize">{{ rd.name }}</span> <a v-if="rd.releaseUrl"
+                                                                                      :href="rd.releaseUrl"
+                                                                                      target="_blank"
+                                                                                      rel="noreferrer noopener">{{
+                                    rd.releaseVersion
+                                }} <v-icon
+                                        x-small>mdi-open-in-new</v-icon></a>
                                 last updated on {{ new Date(rd.lastUpdated).toLocaleDateString() }}.
-                                <a v-if="rd.uri" :href="rd.uri" target="_blank" rel="noreferrer noopener">link <v-icon x-small>mdi-open-in-new</v-icon></a>
+                                <a v-if="rd.uri" :href="rd.uri" target="_blank" rel="noreferrer noopener">link <v-icon
+                                        x-small>mdi-open-in-new</v-icon></a>
                                 <br>
                             </span>
                         </template>
@@ -55,7 +82,7 @@ export default {
     };
   },
   computed: mapState({
-    root: state => state.api.root.data
+    root: state => state.api.root?.data || { externalDatabases: [] }
   }),
   methods: {
     m2h(m) {
@@ -63,7 +90,9 @@ export default {
     }
   },
   created() {
-    this.$store.dispatch("api/getRoot");
+    this.$store.dispatch("api/getRoot").catch(e => {
+      console.error("Failed to retrieve the root endpoint: " + e.message + ".", e);
+    });
   }
 };
 </script>
