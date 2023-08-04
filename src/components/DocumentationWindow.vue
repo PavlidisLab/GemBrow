849551c9f5c0c66b218a5d03993a356a4fcfdd54
Libby Natola <libby.natola@msl.ubc.ca>
2023-08-04 11:29:08 -0700

    Add text for documentation window, first draft no feedback yet

--- src/components/DocumentationWindow.vue
+++ src/components/DocumentationWindow.vue
@@ -9,18 +9,22 @@
                 </v-btn>
             </v-card-title>
             <v-card-text>
-                <p>
-                    How on earth do these filters work?
-                </p>
-                <p>
-                    What are these buttons for?
-                </p>
-                <p>
-                    the limits to how many things are shown. Something like “Within each annotation category, items with small numbers of occurrences (i.e. less than ~4) may not be shown; use the ‘search’ functionality if something you seeking isn’t listed”
-                </p>
-                <p>
-                    We might also need to explain where these annotations come from and that they have limitations like what I said about every dataset having some “biological sex” but not annotated. Something like “The categories and terms shown are derived from the concepts associated with datasets. Datasets are annotated to capture the most salient features of the experimental setting and design” (not sure the best way to say this without giving an example - but we want to keep it short!!)
-                </p>
+                <h3> Filters </h3>
+                <h4 style="font-weight: bolder">How do GemBrow's filter options work?</h4>
+                    <p> There are three main categories you can use to filter data: Taxa, Technology Type, and Annotations, which is further divided into multiple groupings. </p>
+                    <p><strong>AND Filters:</strong> When multiple filters <em>between</em> the major categories and Annotation groupings are selected they act as AND filters. For example, if you select both "Human" in the Taxa category and "Alzheimer's Disease" in the Annotations category's "Disease" grouping, the results will include experiments that involve human subjects AND are annotated with "Alzheimer's Disease". Different groupings within Annotations are also represented by AND clauses (e.g., experiments must contain tags from both "Breast Cancer" from Disease grouping AND "BRCA1" from Genotype grouping). Selecting these features will reduce the number of datasets you see in the table. </p>
+                    <p><strong>OR Filters:</strong> Selection of multiple filters <em>within</em> the categories Taxa, Technology Type, and all Annotation groupings represent OR relationships. For example, a selection of both "Human" and "Mouse" within Taxa category will result in a list of experiments that involve either human OR mouse. Adding multiple OR clauses will increase the number of experiments you see in the table. </p>
+                <h4> What do the numbers show? </h4>
+                    <p> The values displayed next to each selection option represent the number of experiments that contain this term <em>subject</em> to all other category selections, but not within-category selections (e.g., if "Human" is selected in the Taxa category, the number next to "Alzheimer Disease" in the Disease Annotation grouping represents the count of experiments relating to "Alzheimer Disease" in humans within Gemma, and will not change subject to a selection of "Parkinson Disease"). Within each Annotation grouping, items with small numbers of occurrences (i.e. less than ~4) may not be shown; please use the ‘search’ functionality if something you are seeking isn’t listed. The value in the top bar represents the number of experiments that pass the selection filters. Note that because the categories and annotations within Gemma are not mutually exclusive (e.g., a study can contain both "Schizophrenia" and "Autism Spectrum Disorder" annotations), this value may vary slightly from the sum of the values displayed next to filter selection options. </p>
+                <h4> Where do the annotations come from? </h4>
+                    <p> The categories and terms shown are derived from the concepts associated with datasets. Datasets are annotated to capture the most salient features of the experimental setting and design (e.g., the treatments applied to certain parts of organisms at specific life stages to study particular diseases).</p>
+                <br>
+                <h3>Buttons</h3>
+                    <p> When filtering selections have been made, a summary of the filter categories used will be displayed in the top bar of GemBrow. To view a more detailed description of the selections, click this text <v-btn plain style="text-transform: none;" small> Filters applied: <v-icon>mdi-chevron-down</v-icon></v-btn>. Click anywhere to exit the filter description menu.</p>
+
+                    <p> To download a .tsv file with the data from current selections, click this <v-btn plain small><v-icon>mdi-download</v-icon></v-btn>icon. Click <v-btn plain small><v-icon>mdi-cancel</v-icon></v-btn> icon to cancel downloads. </p>
+
+                    <p> Some users may analyse Gemma data in other applications. Open this window <v-btn plain small style="text-transform: none;">Dataset Download Code <v-icon>mdi-chevron-up</v-icon></v-btn>,  select a tab, and use the <v-btn plain small><v-icon>mdi-clipboard-outline</v-icon></v-btn> icon to copy code that downloads the current filtered dataset with python, R, curl, or HTTP. Click anywhere to exit the menu.</p>
             </v-card-text>
         </v-card>
     </v-dialog>
@@ -53,3 +57,9 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+h3 {
+    text-transform: uppercase;
+    }
+</style>
