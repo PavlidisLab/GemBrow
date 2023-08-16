0f5bed892978435fbca3ed423b6ec88d97a11182
Libby Natola <libby.natola@msl.ubc.ca>
2023-08-16 10:25:54 -0700

    Swap text referring to GemBrow with browser in documentation window because I just realized GemBrow is an internal name only

--- src/components/DocumentationWindow.vue
+++ src/components/DocumentationWindow.vue
@@ -2,7 +2,7 @@
     <v-dialog :value="value" @input="newVal => $emit('input', newVal)" width="800">
         <v-card>
             <v-card-title>
-                GemBrow Documentation
+                Documentation
                 <v-spacer></v-spacer>
                 <v-btn icon @click="$emit('input', false)">
                     <v-icon>mdi-close</v-icon>
@@ -10,7 +10,7 @@
             </v-card-title>
             <v-card-text>
                 <h3 > Filters </h3>
-                <h4 style="font-weight: normal">How do GemBrow's filter options work?</h4>
+                <h4 style="font-weight: normal">How do the browser's filter options work?</h4>
                     <p> There are three main categories you can use to filter data: Taxa, Technology Type, and Annotations. Annotations is further divided into groupings which behave similarly to categories. </p>
                     <p><strong>AND Filters:</strong> When multiple filters <em>between</em> the major categories and Annotation groupings are selected they act as AND filters. For example, if both "Human" in the Taxa category and "Alzheimer's Disease" in the Annotations category's "Disease" grouping are selected, the results will include experiments that involve human subjects AND are annotated with "Alzheimer's Disease". Different groupings within Annotations are also represented by AND clauses (e.g., experiments filtered by both "Breast Cancer" from Disease grouping AND "BRCA1" from Genotype grouping must contain both tags). Selecting these features will reduce the number of datasets displayed in the table. </p>
                     <p><strong>OR Filters:</strong> Selection of multiple filters <em>within</em> the categories Taxa, Technology Type, and all Annotation groupings represent OR relationships. For example, a selection of both "Human" and "Mouse" within Taxa category will result in a list of experiments that involve either human OR mouse. Adding multiple OR clauses will increase the number of experiments you see in the table. </p>
@@ -20,7 +20,7 @@
                     <p> The categories and terms shown are derived from the concepts associated with datasets. Datasets are annotated to capture the most salient features of the experimental setting and design (e.g., the treatments applied to certain parts of organisms at specific life stages to study particular diseases).</p>
                 <br>
                 <h3>Buttons</h3>
-                    <p> When filtering selections have been made, a summary of the filter categories used will be displayed in the top bar of GemBrow. To view a more detailed description of the selections, click this text <v-btn plain style="text-transform: none;" small> Filters applied: <v-icon>mdi-chevron-down</v-icon></v-btn>. Click anywhere to exit the filter description menu.</p>
+                    <p> When filtering selections have been made, a summary of the filter categories used will be displayed in the top bar of the browser. To view a more detailed description of the selections, click this text <v-btn plain style="text-transform: none;" small> Filters applied: <v-icon>mdi-chevron-down</v-icon></v-btn>. Click anywhere to exit the filter description menu.</p>
 
                     <p> To download a .tsv file with the data from current selections, click this <v-btn plain small><v-icon>mdi-download</v-icon></v-btn>icon. Click <v-btn plain small><v-icon>mdi-cancel</v-icon></v-btn> icon to cancel downloads. </p>
 
