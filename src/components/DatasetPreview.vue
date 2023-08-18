1057fca240dd1264992a839823de8a33f2b37cb9
Libby Natola <libby.natola@msl.ubc.ca>
2023-08-18 16:37:43 -0700

    Get termName data from datasets/annotations API endpoint, add as chips on datasetPreview

--- src/components/DatasetPreview.vue
+++ src/components/DatasetPreview.vue
@@ -1,18 +1,29 @@
 <template>
     <div class="py-3">
         <h3>{{ dataset.name }}</h3>
-        <span v-html="this.description"></span>
+        <v-chip v-for="term in terms" :key="term.termUri">{{ term.termName }}</v-chip> 
+        <!--* 
+          * Add the termName and parentName values as chips
+          * Color code the chips to correspond to categories (which categories?)
+          * Add termName/name of chip to search query on click
+        -->
+        <div v-html="this.description"></div>
     </div>
 </template>
 
 <script>
 import { highlight } from "@/search-utils";
-import { marked } from "@/config/gemma";
+import { marked, axiosInst, baseUrl } from "@/config/gemma";
 
 export default {
   name: "DatasetPreview",
   props: {
-    dataset: Object
+    dataset: Object    
+  },
+  data() {
+    return {
+      terms: []
+    }
   },
   computed: {
     description() {
@@ -21,6 +32,37 @@ export default {
       }
       return marked.parseInline(this.dataset.description);
     }
+  },
+  methods: {
+    getTerms() {
+    const dataset = this.dataset.id
+    console.log('dataset', dataset)
+    return axiosInst.request({
+      method: 'GET',
+        url: baseUrl + `/rest/v2/datasets/annotations`,
+        params: {
+          filter: `id = ${dataset}`
+        }
+      })
+      .then(response => {
+        console.log('datasets/annotations endpoint data', response.data);
+        return response.data.data;
+      }).catch(error => {
+        console.error('Failed to get datasets/annotations endpoint data', error);
+      });
+   }
+  },
+  created() {
+    /** 
+     * Dispatch the API request when the user opens the dataset preview window 
+     * Query the dataset/annotation endpoint (getTerms())
+     * store termName for selected experiment
+     * store the Name for any of the parent terms that are not selected as a chip below the title
+    */ 
+   console.log('this.dataset', this.dataset)
+   this.getTerms().then(terms => {
+    this.terms = terms;
+   });
   }
 };
 </script>
